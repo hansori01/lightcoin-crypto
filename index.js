e3a5461d762173ae175c17329a1a548730b0e3f4
hansori01 <19sorihan88@gmail.com>
2021-02-12 23:29:35 +0000

    added deposit and withdrawl methods and set transaction superclass

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
+  }
+};
 
-class Withdrawal {
-
-  constructor(amount) {
+// transaction superclass
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
+};
 
+//deposit object class
+class Deposit extends Transaction {
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.amount;
   }
+};
 
-}
+//withdraw object class
+class Withdrawal extends Transaction {
+  commit() {
+    this.account.balance -= this.amount;
+  }
+};
 
 
 
 
 // DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+// console.log(myAccount)
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Transaction 1: Withdrew ', t1.amount + ' from ' + myAccount.username + `. \n\t\t\t\t\t\t Current Balance: ${myAccount.balance}.`);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Transaction 2: Withdrew', t2.amount + ' from ' + myAccount.username + `. \n\t\t\t\t\t\t Current Balance: ${myAccount.balance}.`);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3: Deposited', t3.amount + ' to ' + myAccount.username + `. \n\t\t\t\t\t\t Current Balance: ${myAccount.balance}.`);
+
+console.log('Balance:', myAccount.balance);
